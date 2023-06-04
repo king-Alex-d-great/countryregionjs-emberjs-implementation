db0544b3ed484b585f769fe71f318010d77dece1
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-06-04 05:46:28 +0100

    Bugfix: clean up deleted path routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,10 +47,5 @@ module.exports = {
       },
       extends: ['plugin:n/recommended'],
     },
-    {
-      // test files
-      files: ['tests/**/*-test.{js,ts}'],
-      extends: ['plugin:qunit/recommended'],
-    },
   ],
 };
--- testem.js
+++ testem.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
